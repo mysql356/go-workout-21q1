fee01468e8b7eb266c98750c3d7c2b62d3d2beec
mysql356 <manoj.mysql@gmail.com>
2021-05-09 06:25:23 +0530

    Update e36-channel-loop-sleep.go

--- e36-channel-loop-sleep.go
+++ e36-channel-loop-sleep.go
@@ -22,7 +22,7 @@ func main() {
 }
 
 func hello(i int, done chan int) {
-
+        //to maintain the order of execution
 	var j time.Duration = time.Duration(i)
 	time.Sleep(j * 250 * time.Millisecond)
 	fmt.Println("Hello ----", i, "---", j)
